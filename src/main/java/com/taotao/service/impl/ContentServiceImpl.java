fe5772cd3557bfcd41abd15c587882f88aab554c
Administrator <957986543@qq.com>
2018-12-16 18:25:23 +0800

    dubbo配置

--- src/main/java/com/taotao/service/impl/ContentServiceImpl.java
+++ src/main/java/com/taotao/service/impl/ContentServiceImpl.java
@@ -1,9 +1,13 @@
 package com.taotao.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.comm.util.JsonUtils;
@@ -22,6 +26,9 @@ public class ContentServiceImpl implements ContentService {
 	
 	@Autowired
 	JedisClient jedisClient;
+	
+	@Value("${PAGE_LIST_ID}")
+	private String PAGE_LIST_ID;
 	
 	@Override
 	public List<TbContent> getContentList(long contentCategoryId) {
@@ -45,6 +52,49 @@ public class ContentServiceImpl implements ContentService {
 			e.printStackTrace();
 		}
 		return selectByExample;
+	}
+
+	@Override
+	public String getPageContentList() {
+		try {
+			String hget = jedisClient.hget("常量", PAGE_LIST_ID+"");
+			if(StringUtils.isNotEmpty(hget)) {
+				List<TbContent> cashTbContents = JsonUtils.jsonToList(hget, TbContent.class);
+				return TBlistToString(cashTbContents);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		TbContentExample example = new TbContentExample();
+		Criteria createCriteria = example.createCriteria();
+		createCriteria.andCategoryIdEqualTo(Long.parseLong(PAGE_LIST_ID));
+		List<TbContent> selectByExample = tbContentMapper.selectByExample(example);
+		try {
+			jedisClient.hset("常量", PAGE_LIST_ID+"", JsonUtils.objectToJson(selectByExample));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return TBlistToString(selectByExample);
+	}
+
+	public String TBlistToString(List<TbContent> jsonToList) {
+		if(null!=jsonToList && !jsonToList.isEmpty()) {
+			List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+			for(TbContent j : jsonToList) {
+				Map<String,Object> map = new HashMap<String,Object>();
+				map.put("srcB", j.getPic());
+				map.put("height", 240);
+				map.put("alt", "");
+				map.put("width", 670);
+				map.put("src", j.getPic());
+				map.put("widthB", 550);
+				map.put("href", j.getPic());
+				map.put("heightB", 240);
+				list.add(map);
+				return JsonUtils.objectToJson(list);
+			}
+		}
+		return null;
 	}
 
 }
--- src/test/java/com/DubboProvider.java
+++ src/test/java/com/DubboProvider.java
@@ -6,20 +6,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DubboProvider {
 	
 	public static void main(String[] args) {
-		try {
-			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/application*.xml");
-			context.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		synchronized (DubboProvider.class) {
-			while (true) {
-				try {
-					DubboProvider.class.wait();
-				} catch (InterruptedException e) {
-				}
-			}
-		}
+//		try {
+//			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/application*.xml");
+//			context.start();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		synchronized (DubboProvider.class) {
+//			while (true) {
+//				try {
+//					DubboProvider.class.wait();
+//				} catch (InterruptedException e) {
+//				}
+//			}
+//		}
 	}
 
 }
